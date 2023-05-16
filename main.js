ca7afa8a22b9cfe9883bfb0659d519d59dc4d897
goyal19 <101124248+goyal19@users.noreply.github.com>
2023-05-16 15:40:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+nosex=0;
+nosey=0;
+difference=0;
+leftwristx=0
+rightwristx=0
+
+function setup(){
+   
+
+    video= createCapture(VIDEO);
+    video.size(550,500);
+
+    canvas= createCanvas(550,500)
+    canvas.position(700,100);
+
+    poseNet=ml5.poseNet(video,modelloaded);
+    poseNet.on('pose',gotposes);
+}
+function modelloaded(){
+    console.log("posenet is initialized")
+}
+
+function gotposes(results){
+    if(results.length> 0){
+        console.log(results);
+        nosex=results[0].pose.nose.x;
+        nosey=results[0].pose.nose.y;
+
+        leftwristx=results[0].pose.leftWrist.x;
+        rightwristx=results[0].pose.rightWrist.x;
+        difference=floor(leftwristx-rightwristx)  
+    }
+}
+
+function draw(){
+    background("turquoise")
+
+    fill("#0eebe3");
+    stroke("#0e3eeb");
+    square(nosex,nosey,difference)
+}
+
